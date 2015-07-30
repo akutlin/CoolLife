8aee6d32b708c78c03fd91615e44bf4d9f5b158f
Anton Kutlin <kutawell95@mail.ru>
2015-07-30 19:36:42 +0300

    Maps added for visualising of complex topologies
    Signed-off-by: Anton Kutlin <kutawell95@mail.ru>

--- src/coollife/VisualMapper.java
+++ src/coollife/VisualMapper.java
@@ -7,37 +7,56 @@ import firststep.DoubleXY;
 
 public abstract class VisualMapper {
 		
-	protected Topology tp; 
+	protected final Topology tp;
 	
-	protected abstract void mapIt( double[] p, DoubleXY pos );
 	public abstract void drawAtlas(Canvas cnv, int winWidth, int winHeight);
 	
 	public VisualMapper( Topology tp ) {
 		this.tp = tp;
 	}
 	
-	protected void drawGeodesic( Canvas cnv, Color c, double[] p, double[] v, double length, double step ) {
+	protected abstract class Map {
 		
-		if ( cnv == null || c == null || p == null || v == null) {
-			throw new NullPointerException();
+		protected DoubleXY relativePos, relativeSize;
+		
+		public Map(DoubleXY relativePos, DoubleXY relativeSize) {
+			this.relativePos = relativePos;
+			this.relativeSize = relativeSize;
 		}
 		
-		if ( length < 0 || step < 0 ) {
-			throw new IllegalArgumentException();
+		protected abstract void it( double[] p, DoubleXY pos ) throws MapMismatchException;
+		public abstract void drawMap();
+		
+		protected void drawGeodesic( Canvas cnv, Color c, double[] p, double[] v, double length, double step) {
+			
+			if ( cnv == null || c == null || p == null || v == null) {
+				throw new NullPointerException();
+			}
+			
+			if ( length < 0 || step < 0 ) {
+				throw new IllegalArgumentException();
+			}
+			
+			DoubleXY p0 = new DoubleXY(0, 0);
+			it(p, p0);
+			cnv.beginPath();
+			cnv.strokeColor(c);
+			cnv.moveTo( (float)p0.getX(), (float)p0.getY() );
+			for( double current = 0; current < length; current += step ) {
+				tp.translate(p, v, step);
+				it(p, p0);
+				cnv.lineTo((float)p0.getX(), (float)p0.getY());
+			}
+			
+			cnv.stroke();
 		}
 		
-		DoubleXY p0 = new DoubleXY(0, 0);
-		mapIt(p, p0);
-		cnv.beginPath();
-		cnv.strokeColor(c);
-		cnv.moveTo( (float)p0.getX(), (float)p0.getY() );
-		for( double current = 0; current < length; current += step ) {
-			tp.translate(p, v, step);
-			mapIt(p, p0);
-			cnv.lineTo((float)p0.getX(), (float)p0.getY());
+		protected class MapMismatchException extends RuntimeException {
+			public MapMismatchException() {
+				super();
+			}
+			private static final long serialVersionUID = 8612909242053861560L;
 		}
-		
-		cnv.stroke();
 	}
 
 }
--- src/coollife/classicalConwayModel/DigitalFlatSquareThorMapper.java
+++ src/coollife/classicalConwayModel/DigitalFlatSquareThorMapper.java
@@ -8,6 +8,7 @@ import firststep.DoubleXY;
 public class DigitalFlatSquareThorMapper extends VisualMapper {
 	
 	private final int[] size;
+	private Canvas cnv;
 	private float k, x0, y0;
 	
 	public DigitalFlatSquareThorMapper( DigitalFlatSquareThor tp ) {
@@ -27,15 +28,137 @@ public class DigitalFlatSquareThorMapper extends VisualMapper {
 	    y0 = winHeight / 2 - (size[1] * k / 2);
 	}
 	
-	@Override
-	protected void mapIt(double[] p, DoubleXY pos) {
-		tp.transform(p);
-		pos.setX(x0 + k * p[0]);
-		pos.setY(y0 + k * p[1]);
+	private abstract class GeneralMap extends Map {
+
+		public GeneralMap(DoubleXY relativePos, DoubleXY relativeSize) {
+			super(relativePos, relativeSize);
+			// TODO Auto-generated constructor stub
+		}
+		
+        protected int X0 = (int) (size[0] * relativePos.getX());
+        protected int Y0 = (int) (size[1] * relativePos.getY());
+        protected int width = (int) (size[0] * relativeSize.getX());
+        protected int hight = (int) (size[1] * relativeSize.getY());
+
+		@Override
+		public void drawMap() {
+			
+			//draw frame
+			cnv.beginPath();
+	        cnv.strokeColor(new Color(200, 0, 0));
+			cnv.rect(	x0 + X0 * k,
+						y0 + Y0 * k, 
+						width * k,
+						hight * k);
+	        cnv.stroke();
+			
+			//draw lines
+	        double[] p = new double[2];
+	        double[] vx = new double[2];
+	        double[] vy = new double[2];
+	        Color c = new Color(64, 64, 64, 255);
+	        
+			for ( int i = X0 + 1; i < X0 + width; i++ ) {
+				p[0] = i; p[1] = Y0;
+				vy[0] = 0; vy[1] = 1;
+				drawGeodesic(cnv, c, p , vy , hight, 1);
+			}
+			for ( int i = Y0 + 1; i < Y0 + hight; i++) {
+				p[0] = X0; p[1] = i;
+				vx[0] = 1; vx[1] = 0;
+				drawGeodesic(cnv, c, p , vx , width, 1);
+			}
+		}
+	}
+	
+	private class FirstMap extends GeneralMap {
+
+		public FirstMap() {
+			super(new DoubleXY(0.5,0), new DoubleXY(0.5,0.5));
+		}
+
+		@Override
+		protected void it(double[] p, DoubleXY pos) {
+			tp.transform(p);
+			if ( p[0] >= X0 && p[1] <= Y0 + hight ) { 
+				pos.setX(x0 + k * p[0]);
+				pos.setY(y0 + k * p[1]);
+			} else if( p[0] == 0 && p[1] <= Y0 + hight ) {
+				pos.setX(x0 + k * size[0]);
+				pos.setY(y0 + k * p[1]);
+			} else
+				throw new MapMismatchException();
+		}
+	}
+	
+	private class SecondMap extends GeneralMap {
+
+		public SecondMap() {
+			super(new DoubleXY(0,0), new DoubleXY(0.5,0.5));
+		}
+
+		@Override
+		protected void it(double[] p, DoubleXY pos) {
+			tp.transform(p);
+			if ( p[0] <= width &&
+					p[1] <= hight ) { 
+				pos.setX(x0 + k * p[0]);
+				pos.setY(y0 + k * p[1]);
+			} else
+				throw new MapMismatchException();
+		}
+	}
+	
+	private class ThirdMap extends GeneralMap {
+
+		public ThirdMap() {
+			super(new DoubleXY(0,0.5), new DoubleXY(0.5,0.5));
+		}
+
+		@Override
+		protected void it(double[] p, DoubleXY pos) {
+			tp.transform(p);
+			if ( p[0] <= width && p[1] >= Y0 ) { 
+				pos.setX(x0 + k * p[0]);
+				pos.setY(y0 + k * p[1]);
+			} else if( p[1] == 0 && p[0] <= width ) {
+				pos.setX(x0 + k * p[0]);
+				pos.setY(y0 + k * size[1]);
+			} else
+				throw new MapMismatchException();
+		}
+	}
+	
+	private class FourthMap extends GeneralMap {
+
+		public FourthMap() {
+			super(new DoubleXY(0.5,0.5), new DoubleXY(0.5,0.5));
+		}
+
+		@Override
+		protected void it(double[] p, DoubleXY pos) {
+			tp.transform(p);
+			if ( p[0] >= X0 && p[1] >= Y0 ) { 
+				pos.setX(x0 + k * p[0]);
+				pos.setY(y0 + k * p[1]);
+			} else if ( p[0] == 0 && p[1] >= Y0 ) {
+				pos.setX(x0 + k * size[0]);
+				pos.setY(y0 + k * p[1]);
+			} else if ( p[0] >= X0 && p[1] == 0 ) {
+				pos.setX(x0 + k * p[0]);
+				pos.setY(y0 + k * size[1]);
+			} else if ( p[0] == 0 && p[1] == 0 ) {
+				pos.setX(x0 + k * size[0]);
+				pos.setY(y0 + k * size[1]);
+			} else
+				throw new MapMismatchException();
+		}
 	}
 
 	@Override
 	public void drawAtlas(Canvas cnv, int winWidth, int winHeight) {
+		
+		this.cnv = cnv;
 		
 		fillFrameSize( winWidth, winHeight );
 	    
@@ -45,21 +168,10 @@ public class DigitalFlatSquareThorMapper extends VisualMapper {
         cnv.rect(x0, y0, k * size[0], k * size[1]);
         cnv.fill();
 		
-        // Painting geodesic lines
-        double[] p = new double[2];
-        double[] vx = new double[2];
-        double[] vy = new double[2];
-        Color c = new Color(64, 64, 64, 255);
-		for ( int i = 1; i < size[0]; i++ ) {
-			p[0] = i; p[1] = 0;
-			vy[0] = 0; vy[1] = 1;
-			drawGeodesic(cnv, c, p , vy , size[1], 1);
-		}
-		for ( int j = 1; j < size[1]; j++) {
-			p[0] = 0; p[1] = j;
-			vx[0] = 1; vx[1] = 0;
-			drawGeodesic(cnv, c, p , vx , size[0], 1);
-		}
+        new FirstMap().drawMap();
+        new SecondMap().drawMap();
+        new ThirdMap().drawMap();
+        new FourthMap().drawMap();
 		
 	}
 }
